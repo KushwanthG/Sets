961e2f9ebe50829ca3a63a742003e6c308442e9e
Kushwanth Gondi <36468390+KushwanthG@users.noreply.github.com>
2020-04-08 23:47:43 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,9 @@
+myset=set()
+myset.add(1)
+myset.add(2)
+myset.add(2)
+myset.add(2)
+myset.add(2)
+#adding unique numbers multiply times although it display only single time 
+print(myset)
+print(set(myset))
